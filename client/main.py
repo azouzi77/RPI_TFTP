bbbb6df2ca231c070cf71e4bcdf303ea83559011
Frederick NEY <frederick.ney@gmail.com>
2016-12-10 13:34:38 +0100

    adding main.py

---
+++ client/main.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+__author__ = 'Frederick NEY'
+
+import sys
+from client import Request as App
+
+
+def usage():
+	return
+
+
+def main(argv):
+	hostname = "localhost"
+	port = 5000
+	mode = "get"
+	input_file = None
+	output_file = None
+	for n in range(len(argv)):
+		if argv[n] == "--H" or argv[n] == "--hostname":
+			hostname = argv[n + 1]
+		if argv[n] == "--p" or argv[n] == "-port":
+			port = int(argv[n + 1])
+		if argv[n] == "--m" or argv[n] == "-mode":
+			mode = argv[n + 1]
+		if argv[n] == "--i" or argv[n] == "-input":
+			input_file = argv[n + 1]
+		if argv[n] == "--o" or argv[n] == "-output":
+			output_file = argv[n + 1]
+		if argv[n] == "--h" or argv[n] == "-help":
+			usage()
+	if None == input_file:
+		print("Missing input file\n")
+	if None == output_file:
+		output_file = input_file
+	request = App.Request(hostname, port)
+	request.RequestConnection(input_file, output_file, mode)
+	return
+
+
+if __name__ == '__main__':
+	main(sys.argv)
