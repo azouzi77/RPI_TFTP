841ca49ac1abef1877cd90827158c4fc65c7421b
Frederick NEY <frederick.ney@gmail.com>
2016-12-23 11:58:03 +0100

    adding Get class definition into client directory

---
+++ client/Get.py
@@ -0,0 +1,17 @@
+__author__ = 'Stephan OVERLEN'
+
+
+class Get:
+
+	op_code = None
+
+	error = 0
+
+	def __init__(self, opcode):
+		self.op_code = opcode
+
+	def data_get(self, soket):
+		return
+
+	def get_ack(self, soket):
+		return
