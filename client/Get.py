0440ca0d5a1ed1b753576c81ed35f8c84a2b909a
stephaneoverlen <stephane.overlen@gmail.com>
2016-12-23 12:28:58 +0100

    class get

--- client/Get.py
+++ client/Get.py
@@ -1,17 +1,35 @@
-__author__ = 'Stephan OVERLEN'
+#__author__ = 'Stephan OVERLEN'
 
+import binascii
+import socket
+import struct
+import sys
+
+structSend = struct.Struct('!h 7s h 5s h')
+structData = struct.Struct('!h h 512s')
+structAck = struct.Struct('!h h')
+blocNum = 0
 
 class Get:
 
 	op_code = None
-
 	error = 0
+	socket = None
 
-	def __init__(self, opcode):
+	def __init__(self, opcode, socket):
 		self.op_code = opcode
+		self.socket = socket
 
-	def data_get(self, soket):
-		return
+	def data_get(self):
+	    data = self.soket.recv(structData.size)
+            unpacked_data = structData.unpack(data)                 # get unpacked data
+            if unpacked_data[0] == 0x03:                            # data packet
+                error = False
+            else:                                                   # error packet
+                error = True
+            return unpacked_data[1:2]                               # [blocNum, content]
 
-	def get_ack(self, soket):
-		return
+	def get_ack(self):
+	    data = self.soket.recv(structAck.size)
+            unpacked_data = structData.unpack(data)                 # get unpacked data
+            return unpacked_data[1]
